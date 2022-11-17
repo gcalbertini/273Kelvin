87ce92f087e72f7f88ed7b50c9619534faa437df
hn11235 <hn2231@nyu.edu>
2022-11-17 16:31:57 -0500

    update with added bb model

--- network.py
+++ network.py
@@ -2,8 +2,83 @@
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 #import torch.optim as optim
 from torchvision import models #datasets, transforms, model
+
+class ProjectionHead(nn.Module):
+    #Linear, Batch Norm, ReLU, Linear, Batch Norm
+    def __init__(self,in_features,hidden_features,out_features,**kwargs):
+        super(ProjectionHead,self).__init__(**kwargs)
+        self.in_features = in_features
+        self.out_features = out_features
+        self.hidden_features = hidden_features
+
+        self.layers = nn.Sequential(
+            nn.Linear(self.in_features,self.hidden_features,True),
+            nn.BatchNorm1d(self.hidden_features),
+            nn.ReLU(),
+            nn.Linear(self.hidden_features,self.out_features,False),
+            nn.BatchNorm1d(self.out_features))
+        
+    def forward(self,x):
+        x = self.layers(x)
+        return x
+
+class PreModel(nn.Module):
+    def __init__(self,base_model,labeled):
+        super().__init__()
+        self.base_model = base_model
+        self.labeled = labeled
+        #Add the pretrained model
+        self.pretrained = models.resnet50(pretrained=False)
+        for p in self.pretrained.parameters():
+                p.requires_grad = True
+
+        if labeled:
+            layers = list(self.pretrained.children())[:8]
+            self.features1 = nn.Sequential(*layers[:6])
+            self.features2 = nn.Sequential(*layers[6:])
+            self.classifier = nn.Sequential(nn.BatchNorm1d(512), nn.Linear(512, 4))
+            self.bb = nn.Sequential(nn.BatchNorm1d(512), nn.Linear(512, 4))
+        else:  
+            self.pretrained.conv1 = nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), bias=False)
+            self.pretrained.maxpool = nn.Identity()
+            self.pretrained.fc = nn.Identity()
+            self.projector = ProjectionHead(2048, 2048, 128)
+
+    def forward(self,x):
+        out = self.pretrained(x)
+        if self.labeled:
+            x = self.features1(x)
+            x = self.features2(x)
+            x = F.relu(x)
+            x = nn.AdaptiveAvgPool2d((1,1))(x)
+            x = x.view(x.shape[0], -1)
+            classes = self.classifier(x)
+            bbox = self.bb(x)
+            return classes, bbox
+        else:
+            x_projection = self.projector(torch.squeeze(out))
+            return x_projection
+
+# class BB_model(nn.Module):
+#     def __init__(self):
+#         super(BB_model, self).__init__()
+#         resnet = models.resnet34(pretrained=True)
+#         layers = list(resnet.children())[:8]
+#         self.features1 = nn.Sequential(*layers[:6])
+#         self.features2 = nn.Sequential(*layers[6:])
+#         self.classifier = nn.Sequential(nn.BatchNorm1d(512), nn.Linear(512, 4))
+#         self.bb = nn.Sequential(nn.BatchNorm1d(512), nn.Linear(512, 4))
+        
+#     def forward(self, x):
+#         x = self.features1(x)
+#         x = self.features2(x)
+#         x = F.relu(x)
+#         x = nn.AdaptiveAvgPool2d((1,1))(x)
+#         x = x.view(x.shape[0], -1)
+#         return self.classifier(x), self.bb(x)
 
 # class Identity(nn.Module):
 #     def __init__(self):
@@ -30,46 +105,4 @@ from torchvision import models #datasets, transforms, model
 #         x = self.linear(x)
 #         x = self.bn(x)
 #         return x
-
-class ProjectionHead(nn.Module):
-    #Linear, Batch Norm, ReLU, Linear, Batch Norm
-    def __init__(self,in_features,hidden_features,out_features,**kwargs):
-        super(ProjectionHead,self).__init__(**kwargs)
-        self.in_features = in_features
-        self.out_features = out_features
-        self.hidden_features = hidden_features
-
-        self.layers = nn.Sequential(
-            nn.Linear(self.in_features,self.hidden_features,True),
-            nn.BatchNorm1d(self.hidden_features),
-            nn.ReLU(),
-            nn.Linear(self.hidden_features,self.out_features,False),
-            nn.BatchNorm1d(self.out_features))
-        
-    def forward(self,x):
-        x = self.layers(x)
-        return x
-
-class PreModel(nn.Module):
-    def __init__(self,base_model):
-        super().__init__()
-        self.base_model = base_model
-        
-        #Change pretrained model
-        self.pretrained = models.resnet50(pretrained=False)
-        # self.pretrained.conv1 = nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), bias=False)
-        # self.pretrained.maxpool = Identity()
-        # self.pretrained.fc = Identity()
-        
-        for p in self.pretrained.parameters():
-            p.requires_grad = True
-        
-        self.projector = ProjectionHead(2048, 2048, 128)
-
-    def forward(self,x):
-        out = self.pretrained(x)
-        
-        x_projection = self.projector(torch.squeeze(out))
-        
-        return x_projection
 
