aa5aa6169f1caf73ebe36a91c33972f142a0e136
mrossigrad <mrossi.grad@gmail.com>
2022-11-18 19:09:47 -0500

    completed unlabeled data

--- unlabeled_dataloaders.py
+++ unlabeled_dataloaders.py
@@ -6,18 +6,31 @@ from torchvision import transforms
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
 import numpy as np
+import matplotlib.pyplot as plt
+import torchvision.transforms.functional as F
 
 VALID_DATASET_PATH = "./unlabeled_data/" # this is the path where our images and labels are
 BATCH_SIZE = 2
 NUM_WORKERS = 2
 SHUFFLE = False # whether we want to shiffle the dataset
 
+plt.rcParams["savefig.bbox"] = "tight"
+def show(imgs):
+    if not isinstance(imgs, list):
+        imgs = [imgs]
+    fix, axs = plt.subplots(ncols=len(imgs), squeeze=False)
+    for i, img in enumerate(imgs):
+        img = img.detach()
+        img = F.to_pil_image(img)
+        axs[0, i].imshow(np.asarray(img))
+        axs[0, i].set(xticklabels=[], yticklabels=[], xticks=[], yticks=[])
+
 def collate_fn(batch):
     return tuple(zip(*batch))
 
 transform1 = transforms.Compose([
           transforms.RandomCrop(56),
-          transforms.Resize(112),
+          transforms.Resize(112), # currently the small unlabeled dataset is 3 x 112 x 112
           transforms.ColorJitter(brightness=0.5, hue=.2, saturation=.3, contrast=.2),
           transforms.GaussianBlur(5, sigma=(0.6, 1.0)),
           transforms.ToTensor(),
@@ -25,8 +38,8 @@ transform1 = transforms.Compose([
 
 transform2 = transforms.Compose([
           transforms.RandomCrop(56),
-          transforms.Resize(112),
-          transforms.ColorJitter(brightness=0.9, hue=.2, saturation=.3, contrast=.9),
+          transforms.Resize(112), # currently the small unlabeled dataset is 3 x 112 x 112
+          transforms.ColorJitter(brightness=0.9, hue=.2, saturation=.3, contrast=.7),
           transforms.GaussianBlur(3, sigma=(0.2, 1.0)),
           transforms.ToTensor(),
 ])
@@ -79,3 +92,6 @@ if __name__ == "__main__":
     # 1st image -> has 2 augmentation, to get each augmentation index liek this: batch_images[0][0], batch_images[1][0]
     # 2nd image -> has 2 augmentation, to get each augmentation index liek this: batch_images[0][1], batch_images[1][1]
     batch_images = next(data_loader) 
+    # to display uncomment below
+    show((batch_images[0][0] * 255).to(torch.uint8)) # x_i
+    show((batch_images[1][0] * 255).to(torch.uint8)) # x_j
