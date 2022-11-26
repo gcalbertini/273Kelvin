2ee5886b0ca172394ba9862cb6edaa430f81e27c
hn11235 <hn2231@nyu.edu>
2022-11-26 01:37:34 -0500

    update downstream model

--- network_SimCLR.py
+++ network_SimCLR.py
@@ -43,4 +43,28 @@ class PreModel(nn.Module):
         x_projection = self.projector(torch.squeeze(out))
         return x_projection
 
+model_SimCLR = PreModel('resnet50').to('cuda:0')
+
+class DSModel(nn.Module):
+    def __init__(self,premodel,num_classes):
+        super().__init__()
+        
+        self.premodel = premodel
+        self.num_classes = num_classes
+        
+        for p in self.premodel.parameters():
+            p.requires_grad = False
+            
+        for p in self.premodel.projector.parameters():
+            p.requires_grad = False
+        
+        self.lastlayer = nn.Linear(2048,self.num_classes)
+        
+    def forward(self,x):
+        out = self.premodel.pretrained(x)
+        out = self.lastlayer(out)
+        return out
+
+model_final = DSModel(model_SimCLR, 100)
+
 
