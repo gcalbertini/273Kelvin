999f976f91fec6fd0d3894b191e4ed0793c39d7d
mrossigrad <mrossi.grad@gmail.com>
2022-11-28 16:24:48 -0500

    chnages

--- unlabeled_dataloader.py
+++ unlabeled_dataloader.py
@@ -63,8 +63,8 @@ def unlabeled_dataloader(BATCH_SIZE=4, NUM_WORKERS=2, SHUFFLE=False, DATASET_PAT
             batch_size=BATCH_SIZE,
             shuffle=SHUFFLE,
             num_workers=NUM_WORKERS,
-            #collate_fn=None,
-            #drop_last=True
+            collate_fn=None,
+            drop_last=True
     )
 
     return unlabeled_dataset, unlabeled_dataloader
