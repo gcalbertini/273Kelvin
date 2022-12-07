cf01c5ad7ae162a88ecd749e60960c74f3aebbe6
mrossigrad <mrossi.grad@gmail.com>
2022-12-07 16:04:40 -0500

    final

--- backbone.py
+++ backbone.py
@@ -1,3 +1,4 @@
+'''
 import torch
 import torchvision.models as models
 import torch.nn as nn
@@ -36,3 +37,40 @@ def get_backbone(train=False):
     #return Backbone(backbone)
     #return Backbone(resnet_fpn_backbone('resnet18', weights=checkpoint, trainable_layers=5))
     return resnet_fpn_backbone('resnet18', weights=checkpoint, trainable_layers=5)
+'''
+import torch
+import torchvision.models as models
+import torch.nn as nn
+
+from lightning import train_backbone
+from torchvision.models.detection.backbone_utils import resnet_fpn_backbone
+
+class Backbone(nn.Module):
+    def __init__(self, backbone):
+        super().__init__()
+        self.out_channels = 512
+        self.premodel = backbone
+
+        # change this later
+        #for p in self.premodel.parameters():
+            #p.requires_grad = True
+
+    def forward(self,x):
+        #print(x.size())
+        out = self.premodel(x)
+        #print(out.size())
+        return out
+
+def get_backbone(train=False):
+
+    if train:
+        train_backbone()
+
+    resnet = models.resnet18(pretrained=None)
+    resnet.fc = nn.Identity()
+    checkpoint = torch.load('./resnet18_backbone_weights_final.ckpt')
+    resnet.load_state_dict(checkpoint['model_state_dict'])
+    req_layers = list(resnet.children())[:8]
+    backbone = nn.Sequential(*req_layers)
+
+    return Backbone(backbone)
--- fastrcnn.py
+++ fastrcnn.py
@@ -19,7 +19,7 @@ def get_model(backbone=None, num_classes=100):
     anchor_generator = AnchorGenerator(sizes=anchor_sizes,
                                     aspect_ratios=((0.5, 1.0, 2.0),) * len(anchor_sizes))  # (1:2, 1:1, 2:1); 3 AR + 3 scales scales shown to work best in Faster RCNN paper wrt mAP; probably keep samem
 
-    roi_pooler = torchvision.ops.MultiScaleRoIAlign(featmap_names=['0','1','2','3'],
+    roi_pooler = torchvision.ops.MultiScaleRoIAlign(featmap_names=['0'],
                                                     output_size=7,
                                                     sampling_ratio=2)
 
